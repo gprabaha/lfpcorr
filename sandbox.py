4fe62d2c285a8999aebe758f31b27931c64ffd29
Prabaha <prabaha.gangopadhyay@yale.edu>
2024-03-22 17:58:41 -0400

    adde python env

--- sandbox.py
+++ sandbox.py
@@ -8,3 +8,15 @@ Created on Fri Mar 22 17:44:17 2024
 
 import numpy
 import h5py
+import os
+from scipy.io import loadmat
+
+data_path = "/gpfs/milgram/project/chang/pg496/social_gaze_raw_mat"
+
+fname = "Lynch_Cronenberg_ACCg_BLA_ACCg_01142019-acc.mat"
+
+
+with h5py.File()
+file_path = os.path.join(data_path, fname)
+
+data = loadmat(file_path)
